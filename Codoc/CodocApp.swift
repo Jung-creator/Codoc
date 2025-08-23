4904c306e5f088a9add74521fe083657c197d008
Jung-creator <junchoia25@gmail.com>
2025-08-23 14:39:49 +0900

    Initial Commit

---
+++ Codoc/CodocApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodocApp.swift
+//  Codoc
+//
+//  Created by Choi Jung In on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct CodocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodocTests/CodocTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodocTests.swift
+//  CodocTests
+//
+//  Created by Choi Jung In on 8/23/25.
+//
+
+import Testing
+@testable import Codoc
+
+struct CodocTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
